c37c70266de7efb257d970867157f4fc67dfa565
rafaelsantosxD <43537198+rafaelsantosxD@users.noreply.github.com>
2018-10-15 12:26:42 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApplication3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Coloque aqui o seu codigo
-        
+
+            //Verifique em que quadrante do referencial cartesiano se 
+            //encontra um ponto com as corrdenas (x,y) introduzidas pelo utilizador.
+
+            Console.WriteLine("Insira um valor para x ?");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira um valor para y ?");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Pertence ao primeiro quadrante!");
+            }
+
+            if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Pertence ao quarto quadrante!");
+            }
+            if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Pertence ao segundo quadrante!");
+            }
+            if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Pertence ao terceiro quandrante!");
+            }
+
+            if (x == 0 && y == 0) 
+            {
+                Console.WriteLine("Origem do referencial");
+            }
+
         }
     }
 }
